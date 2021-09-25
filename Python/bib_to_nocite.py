6b32a1c3a0571b8d6587d0d46cd7d202ac532d32
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-09-26 00:15:10 +0100

    finish

--- Python/bib_to_nocite.py
+++ Python/bib_to_nocite.py
@@ -1,2 +1,18 @@
-import pandas as pd
+import re
+
+with open("D:\\Just for Fun\\Python\\ref.bib") as f:
+    lines = f.readlines()
+
+entries = []
+for entry, name in enumerate(lines):
+    if "@" in name:
+        entries.append(name)
+
+for name in range(len(entries)):
+
+    entries[name] = re.sub(r'^.*?{', '', entries[name])
+    entries[name] = entries[name].split(",", 1)[0]
+
+nocite = ',\n'.join(entries)
+print("\\nocite{" + nocite + "}")
 
