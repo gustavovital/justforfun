11e04a5435d29c3abb3fb443c63fb77c93b03c14
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-10-12 19:46:56 +0100

    update df_can

--- Python/Data Visualization with Python/week1.py
+++ Python/Data Visualization with Python/week1.py
@@ -42,5 +42,12 @@ df_can = pd.read_excel(
     skipfooter=2
 )
 
-# df_can.head()
+# data wrangling
 
+df_can.drop(["AREA", 'REG', 'DEV', 'Type', 'Coverage'], axis=1, inplace=True)
+df_can.rename(columns={'OdName': 'Country',
+                       'AreaName': 'Continent',
+                       'RegName': 'Region'}, inplace=True)
+
+df_can['Total'] = df_can.sum(axis=1)
+df_can.set_index('Country', inplace=True)
