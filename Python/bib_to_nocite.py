bb2c91ea067426ccce183ad7c43bed1ecf85f734
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-10-28 10:38:54 +0100

    update bib to no cite

--- Python/bib_to_nocite.py
+++ Python/bib_to_nocite.py
@@ -1,20 +1,25 @@
+# Autor: gustavovital
+# Data:27/10/2021
+#
+# Arquivo .bib para \nocite{}
+
 import re
 
-with open("D:\\Just for Fun\\Python\\refs.bib", encoding="utf8") as f:
-    lines = f.readlines()
-
-entries = []
-for entry, name in enumerate(lines):
-    if "@" in name:
-        entries.append(name)
-
-for name in range(len(entries)):
-
-    entries[name] = re.sub(r'^.*?{', '', entries[name])
-    entries[name] = entries[name].split(",", 1)[0]
-
-nocite = ',\n'.join(entries)
-print("\\nocite{" + nocite + "}")
+# with open("D:\\Just for Fun\\Python\\refs.bib", encoding="utf8") as f:
+#     lines = f.readlines()
+#
+# entries = []
+# for entry, name in enumerate(lines):
+#     if "@" in name:
+#         entries.append(name)
+#
+# for name in range(len(entries)):
+#
+#     entries[name] = re.sub(r'^.*?{', '', entries[name])
+#     entries[name] = entries[name].split(",", 1)[0]
+#
+# nocite = ',\n'.join(entries)
+# print("\\nocite{" + nocite + "}")
 
 # Code with input for the user
 bibfile = input("Select your .bib file")
@@ -34,4 +39,3 @@ for name in range(len(entries)):
 
 nocite = ',\n'.join(entries)
 print("\\nocite{" + nocite + "}")
-
