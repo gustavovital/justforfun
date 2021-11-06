65f5f79915392db467d91232550b33a995045b77
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-11-06 00:03:11 +0000

    add knn file

---
+++ Python/Python for Data Science and Machine Learning/KNN.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.preprocessing import StandardScaler
+
+
+df = pd.read_csv("Python\\Python for Data Science and Machine Learning\\Classified Data", index_col=0)
+# print(df.head(3))
+# print(df.columns)
+
+scaler = StandardScaler()
