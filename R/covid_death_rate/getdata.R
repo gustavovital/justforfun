269c0f0d2f24b5c3fced83b5e7db682716d34337
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-11-25 19:41:47 +0000

    messy in df :/

--- R/covid_death_rate/getdata.R
+++ R/covid_death_rate/getdata.R
@@ -1,13 +1,14 @@
-options(digits.secs = 6)
-start_time <- Sys.time()
+# options(digits.secs = 6)
+# start_time <- Sys.time()
 library(tidyverse)
 library(zoo)
 library(data.table)
 
-
+# Data for mobility ====
 mobility <- fread("https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv", 
                   sep = ",",
-                  select = c('date', 
+                  select = c('date',
+                             'sub_region_1',
                              'country_region_code',
                              'retail_and_recreation_percent_change_from_baseline',
                              'parks_percent_change_from_baseline',
@@ -15,8 +16,50 @@ mobility <- fread("https://www.gstatic.com/covid19/mobility/Global_Mobility_Repo
                              'grocery_and_pharmacy_percent_change_from_baseline',
                              'transit_stations_percent_change_from_baseline',
                              'residential_percent_change_from_baseline'))
-mobility <- mobility %>%  
-  filter(sub_region_1 == "")
+mobility_pt <- mobility %>%  
+  filter(sub_region_1 == "") %>% 
+  filter(country_region_code == "PT")
+
+# Data for vaccinations ====
+vaccination <- fread('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv')
+
+vaccination_pt <- vaccination %>% 
+  filter(iso_code == "PRT")
+
+data_pt <- left_join(mobility_pt, vaccination_pt, by = "date", all = TRUE)
+
+for(na in 1:length(data_pt$people_fully_vaccinated_per_hundred)){
+  if(is.na(data_pt$people_fully_vaccinated_per_hundred[na])){
+    data_pt$people_fully_vaccinated_per_hundred[na] <- 0
+  } else{
+    break
+  }
+}
+
+for(na in 1:length(data_pt$people_vaccinated_per_hundred)){
+  if(is.na(data_pt$people_vaccinated_per_hundred[na])){
+    data_pt$people_vaccinated_per_hundred[na] <- 0
+  } else{
+    break
+  }
+}
+
+# ggplot ====
+
+data_pt %>% 
+  ggplot(aes(x = date)) +
+  # geom_area(aes(y = na.approx(people_fully_vaccinated_per_hundred)), fill = 'dodgerblue2', alpha = .3) 
+  geom_area(aes(y = na.approx(people_vaccinated_per_hundred)), fill = 'dodgerblue', alpha = .3)
+
+vaccination_pt %>% 
+  # filter(iso_code == "BRA") %>%
+  # head()
+  ggplot(aes(x = date)) +
+  geom_area(aes(y = na.approx(people_fully_vaccinated_per_hundred)), fill = 'dodgerblue2', alpha = .3) +
+  geom_area(aes(y = na.approx(people_vaccinated_per_hundred)), fill = 'dodgerblue', alpha = .3)
+
+
+
 
 print(end_time - start_time)
 print(object.size(mobility)/1024)
@@ -33,7 +76,7 @@ mobility %>%
 
 
 mobility %>% 
-  filter(country_region_code == "PT" & sub_region_1 == "") -> mobility_pt
+  filter(country_region_code == "BR" & sub_region_1 == "") -> mobility_br
 
 mobility_pt %>% 
   mutate(roll_mean = rollmean(retail_and_recreation_percent_change_from_baseline, 30, fill = NA)) %>% 
